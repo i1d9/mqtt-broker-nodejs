50fc22f1a2d30ef88700097d980c315f91d31057
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2023-10-01 16:40:23 +0300

    simple mqtt broker

---
+++ index.js
@@ -0,0 +1,75 @@
+const cluster = require('cluster')
+const Aedes = require('aedes')
+const { createServer } = require('net')
+const mq = require('mqemitter')
+const emitter = mq();
+
+function startAedes() {
+    const port = 1883
+
+    const aedes = Aedes({
+        id: 'NAYLAN_BROKER',
+        mq: emitter
+
+    })
+
+    const server = createServer(aedes.handle);
+
+    server.listen(port, 'localhost', function () {
+        console.log('Aedes listening on port:', port)
+        aedes.publish({ topic: 'aedes/hello', payload: "I'm broker " + aedes.id })
+    })
+
+    server.on('error', function (err) {
+        console.log('Server error', err)
+        process.exit(1)
+    })
+
+    aedes.on('subscribe', function (subscriptions, client) {
+        console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
+            '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
+    })
+
+    aedes.on('unsubscribe', function (subscriptions, client) {
+        console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
+            '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
+    })
+
+    // fired when a client connects
+    aedes.on('client', function (client) {
+        console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    })
+
+    // fired when a client disconnects
+    aedes.on('clientDisconnect', function (client) {
+        console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    })
+
+    // fired when a message is published
+    aedes.on('publish', async function (packet, client) {
+        console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
+    });
+
+
+
+    //Listens to an event on the hello topic
+    aedes.mq.on("hello", (d, cb) => {
+        console.log(d.payload.toString());
+        cb();
+    });
+
+
+
+    // Sends an event every 30 seconds
+    setInterval(() => {
+        aedes.mq.emit({
+            topic: "hello",
+            payload: "Hello Client",
+            qos: 0
+        });
+    }, 30 * 1000)
+
+}
+
+
+startAedes();
